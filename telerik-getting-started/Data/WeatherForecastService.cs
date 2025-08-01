fd99002e2f71eb69cc7fe36f655a73800e8f34dc
Shane Kirkbride <shane.kirkbride@gmail.com>
2025-08-01 12:04:36 -0700

    Introduce interface for weather service

--- telerik-getting-started/Data/WeatherForecastService.cs
+++ telerik-getting-started/Data/WeatherForecastService.cs
@@ -1,20 +1,29 @@
 namespace telerik_getting_started.Data
 {
-    public class WeatherForecastService
+    /// <summary>
+    /// Provides random weather forecast data. Implemented as a service so that it can
+    /// be injected where needed.
+    /// </summary>
+    public class WeatherForecastService : IWeatherForecastService
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        /// <inheritdoc />
         public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
+            var forecasts = Enumerable.Range(1, 5)
+                .Select(index => new WeatherForecast
+                {
+                    Date = startDate.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+            return Task.FromResult(forecasts);
         }
     }
 }
--- telerik-getting-started/Program.cs
+++ telerik-getting-started/Program.cs
@@ -4,16 +4,19 @@ using telerik_getting_started.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure services and dependency injection container.
+
+// Register framework and application services.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    // Configure error handling and security headers in production mode.
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
